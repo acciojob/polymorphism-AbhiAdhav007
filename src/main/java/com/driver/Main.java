0f989a54a5dd2726cb19f90c33716493b0973cd1
Abhi Adhav <abhiadhav2843@gmail.com>
2023-01-13 08:13:02 +0530

    3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,20 +10,18 @@ public class Main {
         p.product(1.1, 2.2);
 
     }
+}
+class Product {
 
+    public int product(int x, int y) {
+        return x+y;
+    }
 
-    static class Product {
+    public int product(int x, int y, int z) {
+        return x+y+z;
+    }
 
-        public int product(int x, int y) {
-            return x+y;
-        }
-
-        public int product(int x, int y, int z) {
-            return x+y+z;
-        }
-
-        public double product(double x, double y) {
-            return x+y;
-        }
+    public double product(double x, double y) {
+        return x+y;
     }
 }
